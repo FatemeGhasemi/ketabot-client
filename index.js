21b5eb489adb48784b9ae7e3aaa3ce3eabcf2ec8
fateme <ghasemifateme7320@gmail.com>
2019-01-08 17:19:26 +0330

    Add log for heroku url

--- index.js
+++ index.js
@@ -2,9 +2,10 @@ require('dotenv').config();
 const TelegramBot = require('node-telegram-bot-api');
 let bot;
 if (process.env.NODE_ENV === 'production') {
-    console.log('process.env.HEROKU_URL ',process.env.HEROKU_URL)
     bot = new TelegramBot(process.env.BOT_TOKEN);
-    bot.setWebHook(process.env.HEROKU_URL + bot.token).then(console.log("webHook set for bot"));
+    const webHookUrl = process.env.HEROKU_URL +     bot.token
+    console.log('webHook url  ',webHookUrl)
+    bot.setWebHook(webHookUrl).then(console.log("webHook set for bot"));
 } else {
     bot = new TelegramBot(process.env.BOT_TOKEN, {polling: true});
 }
